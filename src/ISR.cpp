315bd313996615b8e4e37dd754b33cba09ab1adc
riad-BH <riadbenhalla@gmail.com>
2020-01-30 22:20:13 +0100

    Autoformat

--- src/ISR.cpp
+++ src/ISR.cpp
@@ -1,26 +1,26 @@
 /*
- * Copyright (c) 2020 Riad Benhalla [https://github.com/riad-BH/Rate-monotonic] 
- * 
+ * Copyright (c) 2020 Riad Benhalla [https://github.com/riad-BH/Rate-monotonic]
+ *
  * This file is part of Rate-monotonic.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 #include <Arduino.h>
 #include "Def.h"
+
 extern volatile uint8_t flag_tick;
 
-ISR(TIMER1_COMPA_vect){
-flag_tick = ON;
-}
+ISR(TIMER1_COMPA_vect) { flag_tick = ON; }
